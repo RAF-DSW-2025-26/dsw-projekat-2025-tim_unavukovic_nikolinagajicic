3e2f6e21fa8a2948597449191a0a3dd8e59dded7
Nikolina <gajicicnikolina05@gmail.com>
2025-11-07 23:06:04 +0100

    dodata ikona za brisanje,promenu naslova i autora

--- src/main/java/raf/graffito/dsw/controller/RenameAutorAction.java
+++ src/main/java/raf/graffito/dsw/controller/RenameAutorAction.java
@@ -16,7 +16,7 @@ public class RenameAutorAction extends AbstactGraffAction {
     public RenameAutorAction() {
         putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(
                 KeyEvent.VK_N, ActionEvent.CTRL_MASK));
-        putValue(SMALL_ICON, loadIcon("/images/plus.png"));
+        putValue(SMALL_ICON, loadIcon("/images/author.png"));
         putValue(NAME, "Rename author");
         putValue(SHORT_DESCRIPTION, "Rename author");
     }
